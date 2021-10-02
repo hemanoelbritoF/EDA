850f8f22050b901c960e52b2320e56260029463a
Samuel Sato <samuelsato2009@gmail.com>
2021-10-01 23:54:43 -0300

    v1 busca

--- BuscaOrdenacao/main.c
+++ BuscaOrdenacao/main.c
@@ -7,10 +7,8 @@
 int main(){
     FILE *CSV=NULL;
     char nome[30];
-    
 
     //----------- ler arquivo
-
     printf("Informe o nome do arquivo CSV: ");
     scanf("%s", nome);
     CSV = Arquivo_AbreLeitura(nome);
@@ -19,14 +17,65 @@ int main(){
     } else {
         printf("Arquivo aberto: %s\n",nome);
     }
-    int N=0;
+    int nLinhas=0;
     char Linha[100];
     while (!feof(CSV)){
-        printf("%d\n",N);
+        //printf("%d\n",nLinhas);
         fgets(Linha,100,CSV);
-        N++;
+        nLinhas++;
     }
 
+    char **Codg;
+    char teste;
+    fseek(CSV, 0, SEEK_SET);
+    //char Linha
+    Codg = malloc(nLinhas*sizeof(char*));
+    fgets(Linha,100,CSV);
+    for(int i=0; i<nLinhas; i++){
+        Codg[i] = malloc(11*sizeof(char));
+        fgets(Codg[i],11,CSV);
+        fgets(Linha,100,CSV);
+        //printf("%s\n",Codg[i]);
+        
+    }
+    
 
+
+
+
+
+
+
+    /*
+    //teste matriz dinamica
+    int **mat;
+    int i,j;
+    int a;
+    //dizzendo tamanho da matriz quadrada
+    scanf("%d", &a);
+
+    //aloca um vetor de ponteiro para ponteiros(linhas)
+    mat = malloc(a * sizeof(int*));
+
+    //aloca as linhas 
+
+    for (i=0; i<a; i++){
+        mat[i] = malloc(a *sizeof(int));
+    }
+    
+    for(i=0;i<a; i++){
+        for(j=0;j<a;j++){
+            mat[i][j]=i+j;
+            printf("%d \n",mat[i][j]);
+        }
+    }
+    
+    for(i=0;i<3;i++){
+        free(mat[i]);
+    }
+    */
+
+
+    
     return 0;
 }
